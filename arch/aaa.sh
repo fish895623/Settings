8391bbdd6dd66d00135e941563ba2f3b78753d8d
bsh <dan990429@gmail.com>
2020-12-26 00:17:00 +0000

    b

--- arch/aaa.sh
+++ arch/aaa.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-# for i in $(seq $#); do
-#     echo $"$i"
-# done
-# while (( "$#" )); do 
-#   echo $1 
-#   shift 
-# done
-
-if [ $1 == '1' ]; then
-    echo ababc
-fi
+a=`echo abbbb`
+if [[ $a == *"bb"* ]]; then echo error; fi
