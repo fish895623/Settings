14a5bc6220a73b9a0bd858d914a9e876a6fab587
bsh <dan990429@gmail.com>
2020-12-26 11:34:06 +0000

    qq

--- arch/chroot.sh
+++ arch/chroot.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 echo "Enter your root passwd..."
-passwd root &
+passwd root &&
 
 echo "Setup Locale..."
-echo "en_US.UTF-8" >> /etc/locale.gen &
-locale-gen &
+echo "en_US.UTF-8" >> /etc/locale.gen &&
+locale-gen &&
 
 echo "Setup Language..."
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
 echo "Setup hostname (default localhost)..."
-read hostname &
+read hostname &&
 if [[ $hostname == '' ]]; then hostname="localhost"; fi
 
 echo -n "Create New User..."
-echo -n "New Username: "; read -r USER    &
-echo -n "Default shell: "; read -r SHELL  &
+echo -n "New Username: "; read -r USER    &&
+echo -n "Default shell: "; read -r SHELL  &&
 if [[ $shell == ""  ]]; then SHELL="/bin/bash"; fi
-useradd -m -g users -G wheel -s $SHELL $USER &
-passwd $USER &
+useradd -m -g users -G wheel -s $SHELL $USER &&
+passwd $USER &&
 
 # Install Bootloader
 echo "Install Bootloader"
-grub-install --target=x86_64-efi --efi-directory=/boot -bootloader-id=arch --recheck &
-grub-mkconfig -o /boot/grub/grub.cfg &
+grub-install --target=x86_64-efi --efi-directory=/boot -bootloader-id=arch --recheck &&
+grub-mkconfig -o /boot/grub/grub.cfg &&
 
 echo "Enable NetworkManager"
 systemctl enable NetworkManager.service
